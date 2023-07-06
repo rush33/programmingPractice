c5e21e817c183d5adf77d05aa68c065f4d7e4ac7
rush.33 <rushadmahmed@gmail.com>
2023-07-06 11:11:34 +0530

    1

--- flatArray.js
+++ flatArray.js
@@ -2,10 +2,11 @@ const nestedArray = [1, [2, 3], [4, [5, 6]]];
 
 const flattenArray = (arr) => {
   let flatted = [];
+  
   function handleFlat(arr) {
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        console.log("PP");
+        handleFlat(arr[i]);
       } else flatted.push(arr[i]);
     }
   }
